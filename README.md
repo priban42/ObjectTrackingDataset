700685dac8d4d38b5b706bf34af15c290752a50e
Vojta <vojtech.priban@gmail.com>
2023-08-09 11:34:22 +0200

    new simulation calibration sample set

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Contains **camera trajectories** generated in blender and a each such trajectory
 Tutorial for installing and setting up moveit for ROS1 and related dependency installation (not finished)
 
 # blender manual
-Tutorial for using the blender files (shared on google drive).
+Tutorial for using the blender files (shared on [google drive](https://drive.google.com/drive/folders/1vQb5gB6xEPnapNKyxUXrRPEnxYLMggzL?usp=sharing)).
 
 # my_test
 A test ros package used for generating and executing robot trajectories from blender_camera_trajectory.csv.
--- calibration_eyeinhand_ros_pkg/scripts/create_aruco_board_picture_3.py
+++ calibration_eyeinhand_ros_pkg/scripts/create_aruco_board_picture_3.py
@@ -3,6 +3,7 @@ Generates a picture to print a new aruco board. These specific parameters (resol
 """
 
 import cv2
+import numpy as np
 
 aruco_dic = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_1000)
 board_size = (10,14)
@@ -13,6 +14,22 @@ board = cv2.aruco.CharucoBoard((board_size[0], board_size[1]), size_chess_square
 margin_size = 2  # margin in pixel wrt boarder of the img
 marker_border = 1  # number of "bits" surrounding each aruco encoding
 
-img_size = (board_size[0]*10 + margin_size*2, board_size[1]*10 + margin_size*2)  # in pixels, better to be proportial to board size
+square_size = 10
+img_size = (board_size[0]*square_size + margin_size*2, board_size[1]*square_size + margin_size*2)  # in pixels, better to be proportial to board size
 img = board.generateImage(img_size, marginSize=margin_size, borderBits=marker_border)
-cv2.imwrite('charuco_board.bmp', img)
+print(type(img))
+print(np.shape(img))
+
+for y in range(board_size[1]):
+
+    if y%2 == 0:
+        # shift left
+        img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size: np.shape(img)[1] - margin_size - square_size] = img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size + square_size: np.shape(img)[1] - margin_size]
+        # adding black square
+        img[margin_size + y*square_size:margin_size + y*square_size + square_size, np.shape(img)[1] - margin_size - square_size: np.shape(img)[1] - margin_size] = img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size + square_size: margin_size + 2*square_size]
+    else:
+        #shift right
+        img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size + square_size: np.shape(img)[1] - margin_size] = img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size: np.shape(img)[1] - margin_size - square_size]
+        # adding black square
+        img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size: margin_size + square_size] = img[margin_size + y*square_size:margin_size + y*square_size + square_size, margin_size + 2*square_size: margin_size + 3*square_size]
+cv2.imwrite('charuco_board_real.bmp', img)
